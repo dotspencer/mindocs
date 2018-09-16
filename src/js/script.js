deb8cf47fb5dcac61c38b8041936288667916fd3
Spencer Smith <dontaskspencer@gmail.com>
2018-09-16 17:57:27 -0600

    Shows more

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 const content = document.querySelector('.content');
 
 (async () => {
-  const endpoints = await fetch('/data/endpoints.json').then(res => res.json());
+  const rand = Math.random().toString();
+  const endpoints = await fetch(`/data/endpoints.json?u=${rand}`).then(res => res.json());
   endpoints.forEach(ep => {
     const endpointDisplay = createEndpointDisplay(ep);
     content.appendChild(endpointDisplay);
@@ -12,7 +13,17 @@ function createEndpointDisplay(endpoint) {
   const { path, method, desc, post_body, example } = endpoint;
 
   const container = createElement('div', ['endpoint-container']);
+
+  // initially visible
   const titleRow = createElement('div', ['title-row']);
+  titleRow.addEventListener('click', function() {
+    const more = this.parentElement.querySelector('.show-more');
+    if (more) {
+      more.classList.toggle('hidden');
+      const img = this.parentElement.querySelector('img.chevron');
+      img.classList.toggle('rotated');
+    }
+  });
 
   const methodTag = createElement('div', ['tag', [method.toLowerCase()]]);
   methodTag.textContent = method;
@@ -31,7 +42,32 @@ function createEndpointDisplay(endpoint) {
   titleRow.appendChild(chev);
 
   container.appendChild(titleRow);
+
+  // initially hidden
+  if (example) {
+    const { success, fail } = example;
+    const more = createElement('div', ['show-more', 'hidden']);
+
+    more.appendChild(createSnippet('Success', success));
+    more.appendChild(createSnippet('Fail', fail));
+
+    container.appendChild(more);
+  }
+
   return container;
+}
+
+function createSnippet(labelText, object = {}) {
+  const snippet = createElement('div', ['snippet']);
+
+  const exampleSuccess = createElement('pre');
+  exampleSuccess.innerHTML = JSON.stringify(object, null, '  ');
+  const label = createElement('h3');
+  label.innerText = labelText;
+
+  snippet.appendChild(label);
+  snippet.appendChild(exampleSuccess);
+  return snippet;
 }
 
 function createElement(type, classes = []) {
